99d64ba8cfc4395124890206f17ced35a071c9a4
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2020-07-26 14:53:56 -0300

    [+] Tela básica de forms da tarefa

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,81 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(MaterialApp(
+main() => runApp(ToDoPi());
+
+class ToDoPi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: ListaTarefas(),
-        appBar: AppBar(
-          title: Text('π TO-DO'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {},
-        ),
+        body: FormularioTarefa(),
       ),
-    ));
+    );
+  }
+}
+
+class FormularioTarefa extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criar Tarefa'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration:
+                  InputDecoration(labelText: 'Tarefa', hintText: 'Título'),
+              keyboardType: TextInputType.text,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+              decoration:
+                  InputDecoration(labelText: 'Detalhes', hintText: 'Descrição'),
+              keyboardType: TextInputType.text,
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: Text('Adicionar'),
+          )
+        ],
+      ),
+    );
+  }
+}
 
 class ListaTarefas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
-      children: <Widget>[
-        ItemTarefa(Tarefa('Fazer MVP de Flutter',
-            'O MVP de Flutter deve ser feito seguindo os padrões arara premium...')),
-        ItemTarefa(Tarefa('Fazer exercícios físicos',
-            'Às 22:00 deve-se começar com 1 hora de exercícios de calistenia, po...')),
-        ItemTarefa(Tarefa('Fazer site Chevals',
-            'O site da Chevals deve ser feito utilizando a tecnologia WordPress, para is...')),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minhas Tarefas'),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTarefa(Tarefa('Fazer MVP de Flutter',
+              'O MVP de Flutter deve ser feito seguindo os padrões arara premium...')),
+          ItemTarefa(Tarefa('Fazer exercícios físicos',
+              'Às 22:00 deve-se começar com 1 hora de exercícios de calistenia, po...')),
+          ItemTarefa(Tarefa('Fazer site Chevals',
+              'O site da Chevals deve ser feito utilizando a tecnologia WordPress, para is...')),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
@@ -50,5 +100,6 @@ class ItemTarefa extends StatelessWidget {
 
 class Tarefa {
   final String titulo, descricao;
+
   Tarefa(this.titulo, this.descricao);
 }
